ae2c4d0bb7a0bd12f01628c25cef39f48c380c25
wangchen <wangchen@cloudwalk.cn>
2017-12-08 18:18:27 +0800

    upate index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="login-container">
-    <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
-      class="card-box login-form">
-      <h3 class="title">系统登录</h3>
+    
+    <div class="login-backgroud">
+      <div class="login-left">
+          <img src="../../assets/yckj.png">        
+      </div>
+      <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
+        class="card-box login-form">
+      
        <el-form-item prop="username">
         <span class="svg-container svg-container_login">
           <icon-svg icon-class="yonghuming" />
@@ -22,7 +27,8 @@
         </el-button>
       </el-form-item>
       <div class='tips'>账号:admin 密码:admin</div>
-    </el-form>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -88,6 +94,7 @@ export default {
   .login-container {
     @include relative;
     height: 100vh;
+    background-image: url('../../assets/signin_bg.png');
     background-color: $bg;
     input:-webkit-autofill {
       -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
@@ -101,6 +108,30 @@ export default {
       padding: 12px 5px 12px 15px;
       color: $light_gray;
       height: 47px;
+    }
+    .login-backgroud{
+      position: absolute;
+      background-image: url('../../assets/singin_form.png');
+      left:50%;
+      top:50%;
+      width: 842px;
+      height:458px;  
+      margin-left:-421px;margin-top:-229px;
+    }
+    .login-left{
+      
+      width:300px;
+      height: 229px;
+      float:left;
+    }
+     .login-form {
+       position: absolute;
+        width:400px;
+        height: 229px;
+        left: 50%;
+        top:50%;
+        margin-top:-110px;
+        
     }
     .el-input {
       display: inline-block;
@@ -130,19 +161,12 @@ export default {
       text-align: center;
       font-weight: bold;
     }
-    .login-form {
-      position: absolute;
-      left: 0;
-      right: 0;
-      width: 400px;
-      padding: 35px 35px 15px 35px;
-      margin: 120px auto;
-    }
+   
     .el-form-item {
       border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
+      background: rgba(255, 255,255, 0.1);
       border-radius: 5px;
-      color: #454545;
+      color: #ffffff;
     }
     .show-pwd {
       position: absolute;
