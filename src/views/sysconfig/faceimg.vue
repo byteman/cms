f81c381191bdd7b1e715b4837a3195b4d6f4a973
xiaosongluo <cqulxs@163.com>
2017-12-18 19:41:30 +0800

    底库人脸管理基本功能

--- src/views/sysconfig/faceimg.vue
+++ src/views/sysconfig/faceimg.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
     <div class="header">
-      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form :inline="true" class="demo-form-inline">
         <el-form-item label="底库选择">
-          <el-input v-model="formInline.name" placeholder="请选择底库"></el-input>
-        </el-form-item>
-
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">底库设定</el-button>
+          <el-select v-model="selectdb" placeholder="请输入相机编号">
+            <el-option
+              v-for="item in dboptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
         </el-form-item>
 
         <el-form-item>
           <el-button plain @click="onAdd">新增人脸</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button plain @click="onRemove">批量导入</el-button>
+          <el-button plain @click="onImport">批量导入</el-button>
         </el-form-item>
       </el-form>
     </div>
 
     <div class="content">
 
-      <el-table :data="list">
+      <el-table :data="list" v-loading="loading" element-loading-text="加载中,请等待">
 
         <el-table-column type="selection" width="55">
         </el-table-column>
@@ -34,8 +37,8 @@
 
         <el-table-column label="操作" width="300" align='center'>
           <template slot-scope="scope">
-            <el-button type="text" size="small">编辑</el-button>
-            <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
+            <el-button @click="handleEdit(scope.row)" type="text" size="small">编辑</el-button>
+            <el-button @click="handleDelete(scope.row)" type="text" size="small">删除</el-button>
           </template>
         </el-table-column>
 
@@ -44,7 +47,6 @@
       <div class="footer">
         <div class="block">
           <el-pagination
-            @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
             :current-page="currentPage"
             layout="prev, pager, next, jumper"
@@ -54,78 +56,211 @@
       </div>
     </div>
 
+    <el-dialog title='底库批量导入人脸' :visible.sync="upload_show" width="30%" center>
+      <el-form ref="form" label-width="100px">
+        <el-form-item label="底库编号">
+          <el-input v-model="upload_form.group_id" readonly="readonly"></el-input>
+        </el-form-item>
+        <el-form-item label="图片压缩包">
+          <el-upload
+            class="upload"
+            :data="upload_form"
+            :limit="1"
+            :action="upload_url"
+            accept=".zip"
+            :before-upload="handleBeforeUpload"
+            :on-success='handleSuccess'
+            :on-exceed="handleExceed"
+            :file-list="upload_file_list">
+            <el-button size="small" type="primary">选择上传（仅支持zip文件）</el-button>
+            <div slot="tip" class="el-upload__tip">{{ upload_message }}</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="upload_show = false">关闭</el-button>
+      </span>
+    </el-dialog>
+
+    <el-dialog :title="face_title" :visible.sync="face_show" width="50%" center>
+      <el-form ref="form" :model="face" label-width="100px">
+
+        <el-col :span="11">
+
+          <el-form-item label="姓名">
+            <el-input v-model="face.name"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-input v-model="face.male"></el-input>
+          </el-form-item>
+          <el-form-item label="生日">
+            <el-input v-model="face.birthday"></el-input>
+          </el-form-item>
+
+        </el-col>
+
+        <el-col :span="11">
+
+          <el-form-item>
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="imageUrl" :src="face_image_url" class="face.avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+
+        </el-col>
+
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="face_show = false">取 消</el-button>
+        <el-button type="primary" @click="onFaceBtnClick">{{face_dlg_btn_name}}</el-button>
+      </span>
+    </el-dialog>
 
   </div>
 </template>
 
 <script>
-  import {GetVideoDebug} from "@/api/sysconfig";
+  import {
+    GetGroup
+  } from "@/api/sysconfig";
 
   export default {
     data() {
       return {
+        loading: false,
         total: 50,
         currentPage: 1,
-        list: [
-          {
-            id: "0001",
-            name: "公司门口",
-            createtime: "2017-11-23",
-            group_type: "白名单"
-          },
-          {
-            id: "0001",
-            name: "公司门口",
-            createtime: "2017-11-23",
-            group_type: "白名单"
-          },
-          {
-            id: "0001",
-            name: "公司门口",
-            createtime: "2017-11-23",
-            group_type: "白名单"
-          },
-          {
-            id: "0001",
-            name: "公司门口",
-            createtime: "2017-11-23",
-            group_type: "白名单"
-          },
-          {
-            id: "0001",
-            name: "公司门口",
-            createtime: "2017-11-23",
-            group_type: "白名单"
-          }
-        ],
-        formInline: {
-          name: ""
-        }
-      };
+        selectdb: '',
+        dboptions: [],
+        list: [],
+        upload_show: false,
+        upload_file_list: [],
+        upload_form: {},
+        upload_url: '',
+        upload_message: '',
+        face_title: '',
+        face_show: false,
+        face: {},
+        face_dlg_btn_name: '',
+        face_image_url: ''
+      }
     },
     created() {
-      console.log("video debug  created");
+      console.log('face image page created')
     },
     mounted() {
+      this.loading = true
+      this.upload_url = process.env.BASE_API + '/system/increaseLib'
+      GetGroup(80001)
+        .then(response => {
+          const tmpList = response.data.data.group_ids
+          tmpList.forEach(function (item) {
+            item.value = item.id
+            item.label = item.id + '(' + item.group_name + ')'
+          })
+          this.dboptions = tmpList
+          console.log(this.dboptions)
+        })
+        .catch(() => {
+        })
+      this.onRefresh()
     },
     methods: {
-      onSubmit: function () {
+      onRefresh() {
+        this.loading = true
+        this.total = 20
+        this.currentPage = 1
+        this.list = [
+          {
+            id: "0001",
+            name: "公司门口",
+            createtime: "2017-11-23",
+            group_type: "白名单"
+          },
+          {
+            id: "0001",
+            name: "公司门口",
+            createtime: "2017-11-23",
+            group_type: "白名单"
+          }]
+        this.loading = false
       },
-      onReset: function () {
+      handleCurrentChange(val) {
+        this.currentPage = val
+        this.onRefresh()
       },
-      onAdd: function () {
+      onAdd() {
+        this.face_title = '底库新增人脸'
+        this.face_show = true
+        this.face = {}
+        this.face_dlg_btn_name = '新 增'
       },
-      onRemove: function () {
+      onImport() {
+        if (this.selectdb !== '') {
+          this.upload_show = true
+          this.upload_form.group_id = this.selectdb
+          this.upload_file_list = []
+        } else {
+          this.$message('底库为空，请选择底库后重试')
+        }
       },
-      handleClick(row) {
-        console.log(row);
+      handleDelete(row) {
+        this.$confirm('确认删除该设备, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+          .then(() => {
+          })
+          .catch(() => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          })
       },
-      handleSizeChange: function () {
+      handleEdit(row) {
+        this.face_title = '底库修改人脸'
+        this.face_show = true
+        this.face = {}
+        this.face_dlg_btn_name = '修 改'
       },
-      handleCurrentChange: function () {
+      handleBeforeUpload(file) {
+        this.upload_message = ''
+      },
+      handleSuccess(res, file, fileList) {
+        if (res.status !== 0) {
+          this.upload_message = '失败反馈:' + JSON.stringify(res)
+        }
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg'
+        const isLt2M = file.size / 1024 / 1024 < 2
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!')
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+        }
+        return isJPG && isLt2M
+      },
+      onFaceBtnClick() {
       }
     }
-  };
+  }
 </script>
 
 <style scoped>
@@ -134,14 +269,12 @@
     margin: 0 auto;
     border: 1px solid #dfe6ec;
     min-height: 600px;
+
   }
 
-  .button-oper {
-  }
+  .el-form {
 
-  .el-form-item {
-    margin-top: 5px;
-    margin-bottom: 5px;
+    overflow: hidden;
   }
 
   .content {
@@ -165,12 +298,30 @@
     margin-right: 90px;
     text-align: right;
   }
-
-  .el-card__header {
-    padding: 1px 2px;
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
   }
 
-  .tree {
-    height: 1000px;
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .face .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 </style>
