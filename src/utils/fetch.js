eab604953e0a71028305f3094c546f2df4f9fd50
wenxue <wenxue@cloudwalk.cn>
2018-01-08 17:11:33 +0800

    publish

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -7,8 +7,8 @@ import { getToken, removeToken } from '@/utils/auth'
 
 // 创建axios实例
 const service = axios.create({
-  // baseURL: 'http://' + location.hostname + ':8000', // api的base_url
-  baseURL: process.env.BASE_API,
+  baseURL: 'http://' + location.hostname + ':8000', // api的base_url
+  // baseURL: process.env.BASE_API,
   timeout: 15000 // 请求超时时间
 })
 
