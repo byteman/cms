2457d1613407984f6c5a383134e4023bccb4517d
litao <litao@cloudwalk.cn>
2018-01-10 13:59:01 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   HTTP_HOST: '192.168.40.176:8000',
   BASE_API: '"http://192.168.40.176:8000"',
-  //BASE_API: 'http://' + location.hostname + ':8000'
+//  BASE_API: 'http://' + location.hostname + ':8000',
 })
